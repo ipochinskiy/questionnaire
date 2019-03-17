b797e3e3e1a3f170c0b06fa396555178531be241
Ilia Pochinskii <ilia.pochinskii@stayfriends.com>
2019-03-17 03:32:05 +0100

    add basic questionnaire component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.scss';
+import Questionnaire from './Questionnaire';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
+                <Questionnaire />
             </div>
         );
     }
---
+++ src/Questionnaire.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Questionnaire.scss';
+
+class Questionnaire extends Component {
+    render() {
+        return (
+            <div className="Questionnaire">
+            </div>
+        );
+    }
+}
+
+export default Questionnaire;
