debda67a7dcaa2706b6d4398b1f248ac9cd4eb25
Ilia Pochinskii <ilia.pochinskii@stayfriends.com>
2019-03-17 11:10:13 +0100

    render the type of the question as a dummy text

--- src/questionnaire/Panel/Panel.js
+++ src/questionnaire/Panel/Panel.js
@@ -5,11 +5,39 @@ class Panel extends Component {
     render() {
         const { question: { key, type, data = {}, isRequired } = {} } = this.props;
         const { question, body, answers } = data;
+        let control;
+        switch (type) {
+            case 'single':
+                control = 'radio';
+                break;
+            case 'select':
+                control = 'select';
+                break;
+            case 'multiple':
+                control = 'multiple';
+                break;
+            case 'textarea':
+                control = 'textarea';
+                break;
+            case 'text':
+                control = 'text';
+                break;
+
+            default:
+                break;
+        }
+
+        if (!control) {
+            return null;
+        }
 
         return (
             <div className='Panel'>
                 {question && <div className='Panel__title'>{question}</div>}
                 {body && <div className='Panel__description'>{body}</div>}
+                <div className='Panel__type'>
+                    {control}
+                </div>
             </div>
         );
     }
--- src/questionnaire/Panel/Panel.test.js
+++ src/questionnaire/Panel/Panel.test.js
@@ -5,29 +5,160 @@ import Panel from './Panel';
 describe('Component: Panel', () => {
     let props;
 
-    beforeEach(() => {
-        props = createComponentProps();
-    });
+    [
+        [
+            'with a question of type "single"',
+            'should render all the radio options',
+            { question: createRadioQuestion() },
+            component => expect(component).toIncludeText('radio'),
+        ],
+        [
+            'with a question of type "select"',
+            'should render all the select options',
+            { question: createSelectQuestion() },
+            component => expect(component).toIncludeText('select'),
+        ],
+        [
+            'with a question of type "multiple"',
+            'should render all the multiple options',
+            { question: createMultiQuestion() },
+            component => expect(component).toIncludeText('multiple'),
+        ],
+        [
+            'with a question of type "textarea"',
+            'should render all the textarea options',
+            { question: createTextareaQuestion() },
+            component => expect(component).toIncludeText('textarea'),
+        ],
+        [
+            'with a question of type "text"',
+            'should render all the text options',
+            { question: createTextQuestion() },
+            component => expect(component).toIncludeText('text'),
+        ],
+    ].forEach(([ description, assumption, props, checkRender ]) => {
+        describe(description, () => {
+            let component;
 
-    it('should render the question', () => {
-        const component = shallow(<Panel {...props} />);
+            beforeEach(() => {
+                component = shallow(<Panel {...props} />);
+            });
 
-        expect(component).toIncludeText(`what's the meaning of life?`);
-        expect(component).toIncludeText(`we'll return to this place in exactly seven and a half million years`);
-        expect(component).toIncludeText('radio');
+            it(assumption, () => {
+
+                expect(component).toIncludeText(`Which Avanger is the best?`);
+                expect(component).toIncludeText(`Choose wise, there's no way back`);
+                checkRender(component);
+            });
+        });
     });
 });
 
-function createComponentProps(options = {}) {
+function createRadioQuestion(options = {}) {
     return {
-        question: {
-            key: 'first question',
-            type: 'single',
-            data: {
-                question: `what's the meaning of life?`,
-                body: `we'll return to this place in exactly seven and a half million years`,
-            },
-            isRequired: true,
+        key: 'single question',
+        type: 'single',
+        data: {
+            question: 'Which Avanger is the best?',
+            body: `Choose wise, there's no way back`,
+            answers: [
+                { value: 'iman', label: 'Iron Man' },
+                { value: 'cap', label: 'Captain America' },
+                { value: 'thor', label: 'Thor' },
+            ],
         },
+        isRequired: true,
+        ...options,
+    };
+}
+
+function createSelectQuestion(options = {}) {
+    return {
+        key: 'select question',
+        type: 'select',
+        data: {
+            question: 'Which Avanger is the best?',
+            body: `Choose wise, there's no way back`,
+            answers: [ 'Iron Man', 'Captain America', 'Thor' ],
+        },
+        isRequired: true,
+        ...options,
+    };
+}
+
+function createMultiQuestion(options = {}) {
+    return {
+        key: 'multiple question',
+        type: 'multiple',
+        data: {
+            question: 'Which Avanger is the best?',
+            body: `Choose wise, there's no way back`,
+            answers: [
+                {
+                    "key": "the_strongest",
+                    "label": "Rate the strength",
+                    "options": [
+                        {
+                            "label": "Iron Man",
+                            "value": "iman"
+                        },
+                        {
+                            "label": "Captain America",
+                            "value": "cap"
+                        },
+                        {
+                            "label": "Thor",
+                            "value": "thor"
+                        },
+                    ],
+                },
+                {
+                    "key": "the_smartest",
+                    "label": "Rate intelligence",
+                    "options": [
+                        {
+                            "label": "Iron Man",
+                            "value": "iman"
+                        },
+                        {
+                            "label": "Captain America",
+                            "value": "cap"
+                        },
+                        {
+                            "label": "Thor",
+                            "value": "thor"
+                        },
+                    ],
+                },
+            ],
+        },
+        isRequired: true,
+        ...options,
+    };
+}
+
+function createTextareaQuestion(options = {}) {
+    return {
+        key: 'textarea question',
+        type: 'textarea',
+        data: {
+            question: 'Which Avanger is the best?',
+            body: `Choose wise, there's no way back`,
+        },
+        isRequired: true,
+        ...options,
+    };
+}
+
+function createTextQuestion(options = {}) {
+    return {
+        key: 'text question',
+        type: 'text',
+        data: {
+            question: 'Which Avanger is the best?',
+            body: `Choose wise, there's no way back`,
+        },
+        isRequired: true,
+        ...options,
     };
 }
